bd498024030e0025f79ba783ba6da616715206fe
Bobbie Smulders <mail@bsmulders.com>
2018-04-15 01:54:22 +0200

    Enable multi-color LED

--- src/main/java/com/bsmulders/cx300control/cx300/control/CX300Led.java
+++ src/main/java/com/bsmulders/cx300control/cx300/control/CX300Led.java
@@ -8,10 +8,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class CX300Led {
 
-    private static final byte[] STATUS_LED_ON = new byte[]{0x16, 0x01};
-    private static final byte[] STATUS_LED_OFF = new byte[]{0x16, 0x00};
-    private static final byte[] SPEAKERPHONE_LED_ON = new byte[]{0x02, 0x01};
-    private static final byte[] SPEAKERPHONE_LED_OFF = new byte[]{0x02, 0x00};
+    private static final byte[] STATUS_LED_OFF          = new byte[]{0x16, 0x00};
+    private static final byte[] STATUS_LED_GREEN        = new byte[]{0x16, 0x01};
+    private static final byte[] STATUS_LED_RED          = new byte[]{0x16, 0x03};
+    private static final byte[] STATUS_LED_YELLOW_RED   = new byte[]{0x16, 0x04};
+    private static final byte[] STATUS_LED_YELLOW       = new byte[]{0x16, 0x05};
+    private static final byte[] SPEAKERPHONE_LED_OFF    = new byte[]{0x02, 0x00};
+    private static final byte[] SPEAKERPHONE_LED_ON     = new byte[]{0x02, 0x01};
 
     private final HidDeviceCommunicator hidDeviceCommunicator;
 
@@ -20,8 +23,20 @@ public class CX300Led {
         this.hidDeviceCommunicator = hidDeviceCommunicator;
     }
 
-    public void enableStatusLed() {
-        hidDeviceCommunicator.sendMessage(STATUS_LED_ON);
+    public void enableGreenStatusLed() {
+        hidDeviceCommunicator.sendMessage(STATUS_LED_GREEN);
+    }
+
+    public void enableRedStatusLed() {
+        hidDeviceCommunicator.sendMessage(STATUS_LED_RED);
+    }
+
+    public void enableYellowStatusLed() {
+        hidDeviceCommunicator.sendMessage(STATUS_LED_YELLOW);
+    }
+
+    public void enableYellowRedStatusLed() {
+        hidDeviceCommunicator.sendMessage(STATUS_LED_YELLOW_RED);
     }
 
     public void disableStatusLed() {
